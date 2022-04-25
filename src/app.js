a54d3845b79a4e4fbb79872ccf67a979ff038b35
[Agnes Font] <[afonttorres@gmail.com]>
2022-04-25 16:17:22 +0200

    list rendered & item status update working

---
+++ src/app.js
@@ -0,0 +1,67 @@
+let name = 'Agnes';
+console.log(typeof name);
+
+let nameArr = ['Agnes', 'Nil', 'Sira', 'Rafa', 'Eva', 'Esther', 'SW']
+for (let item of nameArr) {
+    console.log(item)
+}
+
+const user = {
+    name: 'Agnès',
+    age: 27,
+    msg: `Hi, I'm ${name}`,
+    //mètode
+    saludar: function sayHi() {
+        console.log(this.msg)
+    }
+}
+
+function sumaArray(arr) {
+    let suma = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        let numero = arr[i];
+
+        if (typeof numero === 'number') suma += numero
+    }
+
+    return suma;
+}
+
+function renderToDos(arr){
+    const list = document.getElementById('list');
+    
+    arr.forEach(el => {
+        let listItem = document.createElement('li');
+        listItem.innerText = el;
+        listItem.classList.add('li_unclicked')
+        listItem.onclick = ()=>{
+            listItem.classList.toggle('li_clicked')
+        }
+        list.appendChild(listItem);
+    });
+
+}
+
+function init() {
+    user.saludar();
+
+    let numArray = [2, 5, 6, 4, 88, 21, 37]
+    let resultatSumaArr = sumaArray(numArray)
+    console.log(resultatSumaArr);
+
+    let toDoList = [
+        'Llevar-se',
+        'Treure a passejar el gos',
+        'Fer classe',
+        'Cafè',
+        'Fer classe',
+        'Dinar',
+        'Fer classe',
+        'Anar al gimnàs',
+
+    ]
+    renderToDos(toDoList);
+}
+
+init();
